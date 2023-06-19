cc3594c703c1e1f8d4672795ff7ba22cf18507c7
Gabriel Seidl <gabrielseidl@hotmail.com>
2023-06-19 18:48:13 -0300

    atualizacao dos paginas

--- js/service-worker-register.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/js/service-worker.js')
-        .then(registration => {
-            console.log('Service Worker registrado com sucesso: lololo', registration);
-        })
-        .catch(error => {
-            console.log('Falha ao registrar o Service Worker:', error);
-        });
-}
